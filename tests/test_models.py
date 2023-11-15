da7d831f24548f83b3d44abdcfd5dcd6accd1781
Gabriel-n90 <149002075+Gabriel-n90@users.noreply.github.com>
2023-11-15 01:57:14 +0000

    Delete tests directory

--- tests/test_models.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-from hello_azure.models import MyModel
-
-class MyModelTestCase(TestCase):
-    def test_model_creation(self):
-        obj = MyModel.objects.create(name='Test Name')
-        self.assertEqual(obj.name, 'Test Name')
-        # Add more assertions based on your model's behavior
